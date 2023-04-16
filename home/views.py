e2570cd466bac49b8f0df80ee9a4c54c99519e6f
CamilCar <cc.carsbo@gmail.com>
2023-04-16 13:40:29 +0200

    add contact routing

--- home/views.py
+++ home/views.py
@@ -7,3 +7,9 @@ def index(request):
     """ Returns index page """
 
     return render(request, 'home/index.html')
+
+
+def contact(request):
+    """ Returns contact page """
+
+    return render(request, 'contact/contact.html')
