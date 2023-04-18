3f93d9396b92de0c0ba44034ad38d2c5fa4e66ed
CamilCar <cc.carsbo@gmail.com>
2023-04-18 14:03:19 +0200

    Add 4 new pages with base content and setup routes

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('delivery/', views.delivery, name='delivery'),
+    path('pricing/', views.pricing, name='pricing'),
 ]
--- home/views.py
+++ home/views.py
@@ -13,3 +13,21 @@ def contact(request):
     """ Returns contact page """
 
     return render(request, 'contact/contact.html')
+
+
+def about(request):
+    """ Returns about page """
+
+    return render(request, 'about/about.html')
+
+
+def delivery(request):
+    """ Returns delivery info page """
+
+    return render(request, 'delivery_info/delivery.html')
+
+
+def pricing(request):
+    """ Returns prices page """
+
+    return render(request, 'pricing/pricing.html')
