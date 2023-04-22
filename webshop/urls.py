c2e571fe76d37086f504d8c91fda66515b444bc0
CamilCar <cc.carsbo@gmail.com>
2023-04-22 11:51:07 +0200

    add filter to webshop and add print detail page

--- webshop/urls.py
+++ webshop/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.webshop, name="webshop"),
+    path('<int:pk>/', views.product_detail, name='product_detail'),
 ]
--- webshop/views.py
+++ webshop/views.py
@@ -1,15 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Print, Category
 
 # Create your views here.
 
 
 def webshop(request):
-    """ Returns webshop page """
-    prints = Print.objects.all()
+    """ Returns webshop page, including filter """
+    prints = None
     categories = Category.objects.all()
+
+    if 'category' in request.GET:
+        categoryToFind = request.GET['category']
+        prints = Print.objects.filter(category=categoryToFind)
+    else:
+        prints = Print.objects.all()
 
     return render(request, 'webshop/gallery.html', {
         'categories': categories,
         'prints': prints
+    })
+
+
+def product_detail(request, pk):
+    print = get_object_or_404(Print, pk=pk)
+
+    return render(request, 'webshop/product_detail.html', {
+        'print': print,
     })
