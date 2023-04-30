9966b5d5394be959266cc8e69bdc823b7a507e33
CamilCar <cc.carsbo@gmail.com>
2023-04-30 21:36:18 +0200

    Cleanup from db refactor. Swish logo and style

--- webshop/admin.py
+++ webshop/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Category, Print, Cart, CartItem
+from .models import Category, Print, PrintOption
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Print)
-admin.site.register(Cart)
-admin.site.register(CartItem)
+admin.site.register(PrintOption)
+
--- webshop/urls.py
+++ webshop/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.webshop, name="webshop"),
-    path('product_detail/<int:pk>/', views.product_detail, name='product_detail'),
-    path("add_to_cart", views.add_to_cart, name="add_to_cart"),
+    path('product_detail/<int:pk>/', views.product_detail,
+         name='product_detail'),
 ]
