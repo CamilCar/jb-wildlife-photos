c1ed9472db826d0cf175fdc11ee35e1f397a089f
CamilCar <cc.carsbo@gmail.com>
2023-05-02 02:47:18 +0200

    Complete create_order to handle authentication, price and orders

--- orders/views.py
+++ orders/views.py
@@ -1,15 +1,38 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import login_required
+from cart.contexts import cart_content
+from cart.forms import ConfirmBookingForm
 
-from orders.models import Order
+from orders.models import Order, OrderItem
+from webshop.models import PrintOption
 
 # Create your views here.
 
 
+@login_required
 def create_order(request):
-    if request.user.is_authenticated:
-        print("creating order")
-        cart = request.session.get('cart', {})
-        print(cart)
-    else:
-        print("you must create an account first")
-        return redirect('/home')
+
+    print(request.POST)
+    shipping_information_form = ConfirmBookingForm(request.POST)
+    print(shipping_information_form)
+
+    if shipping_information_form.is_valid():
+        new_shipping_information = shipping_information_form.save()
+        cart = cart_content(request)
+
+        user = request.user
+        new_order = Order(user=user,
+                          shipping_information=new_shipping_information)
+        new_order.save()
+
+        for item in cart['cart_items']:
+            print_option = get_object_or_404(PrintOption, size=item['size'])
+            new_cart_item = OrderItem(print=item['print'],
+                                      amount=item['quantity'],
+                                      print_option=print_option
+                                      )
+            new_cart_item.save()
+            new_order.prints.add(new_cart_item)
+
+        return render(request, 'orders/thanks_for_order.html')
+    return redirect(request.META['HTTP_REFERER'])
