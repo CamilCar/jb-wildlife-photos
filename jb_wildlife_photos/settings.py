dd4ba6716a39f74477ce818b58abdf1c57cd7dbe
CamilCar <cc.carsbo@gmail.com>
2023-05-02 05:43:38 +0200

    Corr to settings

--- jb_wildlife_photos/settings.py
+++ jb_wildlife_photos/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['jbwildlifephotos.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['jbwildlifephotos.herokuapp.com', 'localhost']
 
 
 # Application definition
